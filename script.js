36c15dfa62a5db866e2a7b8287a9903600b16d1c
unknown <kayskill10@gmail.com>
2022-03-24 17:05:02 +0300

    set up a better code and put in a full array of chars

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create a function that returns a random item from the array
 //modify function so that it returns multiple items from the array
 
-const chars = ["a", "b", "c", "d", "e", "A", "B", "?", "/", "!", "+", "@", "U", "x", ")"]
+const chars = `~!@$%^&()_+-=qwertyuiopQWERTYUIOPasdfghjklASDFGHJKLzxcvbnmZXCVBNM<>?`.split('')
 
 const btn = document.getElementById('generate-btn')
 btn.addEventListener("click", (event) => {
@@ -12,12 +12,21 @@ btn.addEventListener("click", (event) => {
     // getRandomChars()
 })
 
-// console.log(chars.length)
+// function getRandomChars() {
+//     let randoms = chars[Math.floor(Math.random() * chars.length)]
+//     // console.log(randoms)
+//     const shuffled = chars.sort(() => 0.5 - Math.random()).slice(0)
+//     console.log(shuffled.join(''))
+// }
 
-function getRandomChars() {
-    let randoms = chars[Math.floor(Math.random() * chars.length)]
-    // console.log(randoms)
-    const shuffled = chars.sort(() => 0.5 - Math.random()).slice(0)
-    console.log(shuffled.join(''))
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)]
 }
 
+function getRandomChars() {
+    result = ''
+    for (let i = 0; i < 8; i++) {
+        result += randomElement(chars)
+    }
+    console.log(result)
+}
