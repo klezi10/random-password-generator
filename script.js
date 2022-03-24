e1c7051cbdecb0aa6cb2170a03298a3c1998200b
unknown <kayskill10@gmail.com>
2022-03-24 16:41:53 +0300

    random password is generated without repeating chars

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 // create a function that returns a random item from the array
 //modify function so that it returns multiple items from the array
 
-const chars = ["rock", "paper", "scissors", "foot", "a", "b", "c", "d"]
+const chars = ["a", "b", "c", "d", "e", "A", "B", "?", "/", "!", "+", "@", "U", "x", ")"]
+
+const btn = document.getElementById('generate-btn')
+btn.addEventListener("click", (event) => {
+    event.preventDefault()
+    getRandomChars()
+    // getRandomChars()
+    // getRandomChars()
+    // getRandomChars()
+})
+
+// console.log(chars.length)
 
 function getRandomChars() {
     let randoms = chars[Math.floor(Math.random() * chars.length)]
     // console.log(randoms)
-    const shuffled = chars.sort(() => 0.5 - Math.random()).slice(4, chars.length)
-    console.log(shuffled)
+    const shuffled = chars.sort(() => 0.5 - Math.random()).slice(0)
+    console.log(shuffled.join(''))
 }
 
-getRandomChars()
