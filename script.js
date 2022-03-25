9f56d84f3569ff8b0f48eb1befda73eb5648d2b7
unknown <kayskill10@gmail.com>
2022-03-25 12:56:04 +0300

    removed flexbox, added grid, fixed code to display four passwords

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
-// create a function that returns a random item from the array
-//modify function so that it returns multiple items from the array
-
-const chars = `~!@$%^&()_+-=qwertyuiopQWERTYUIOPasdfghjklASDFGHJKLzxcvbnmZXCVBNM<>?`.split('')
+const chars = `~!@$%^&()_+-=qwertyuiopQWERTYUIOPasdfghjklASDFGHJKLzxcvbnmZXCVBNM?`
+let newArray = []
 
 const displayPasswords = document.querySelectorAll('.passwords')
 const containerBottom = document.querySelector('.container-bottom')
-// console.log(displayPasswords)
 
 const btn = document.getElementById('generate-btn')
 btn.addEventListener("click", (event) => {
     event.preventDefault()
     getRandomChars()
-    getRandomChars()
-    getRandomChars()
-    getRandomChars()
 })
-
-// function getRandomChars() {
-//     let randoms = chars[Math.floor(Math.random() * chars.length)]
-//     // console.log(randoms)
-//     const shuffled = chars.sort(() => 0.5 - Math.random()).slice(0)
-//     console.log(shuffled.join(''))
-// }
 
 function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)]
 }
 
 function getRandomChars() {
-    let result = ''
+    let firstResult = ''
+    let secondResult = ''
+    let thirdResult = ''
+    let fourthResult = ''
     for (let i = 0; i < 8; i++) {
-        result += randomElement(chars)
+        firstResult += randomElement(chars)
+        secondResult += randomElement(chars)
+        thirdResult += randomElement(chars)
+        fourthResult += randomElement(chars)
     }
-    // console.log(result)
-    // displayFourPasswords(result)
-    // for (let i = 0; i < displayPasswords.length; i++) {
-        // console.log(displayPasswords[i])
-        let newArray = []
-
-
-        newArray.push(result)
-       
-        // console.log(newArray)
-        displayFourPasswords(newArray)
+        newArray.unshift(firstResult, secondResult, thirdResult, fourthResult)
+        displayFourPasswords()
     }
-// }
 
-function displayFourPasswords(newArray) {
+function displayFourPasswords() {
+    let displayPassHTML = `
+    <div class="container-bottom">`
     console.log(newArray)
-    newArray.forEach(element => {
-        console.log(element)
-
-        let displayPassHTML = `
-    <div class="flex-1">
+    newArray.forEach(function(element, index) {
+        if (index < 4) {
+console.log(element)
+        displayPassHTML += `
             <p class="passwords">${element}</p>
-                <p class="passwords">${element}</p>
-            </div>
-            <div class="flex-2">
-                <p class="passwords">${newArray}</p>
-                <p class="passwords">${newArray}</p>
-                </div>
-            `
-        // }
-    // });
+            `;
+        }
+    });
     displayPassHTML += `
     </div>
     `
     containerBottom.innerHTML = displayPassHTML;
-    // console.log(displayPassHTML)
-    });
-    
 }
